8e4da53d4d08f87c9e4b12def79317ea23ddee64
Arturo Nevarez <nevarez.arturo@uabc.edu.mx>
2018-11-17 23:52:10 -0800

    Enviroments.prod.ts
    * Added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6xbgch5roN-_5OiFiGgfnJnbeE7VZGsk",
+    authDomain: "naturalgrown-53f79.firebaseapp.com",
+    databaseURL: "https://naturalgrown-53f79.firebaseio.com",
+    projectId: "naturalgrown-53f79",
+    storageBucket: "naturalgrown-53f79.appspot.com",
+    messagingSenderId: "1036483468434"
+  }
 };
